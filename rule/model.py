79b735e6c0a57ac206ac48deaf5de50da7bb18b9
RiverGao <181240016@smail.nju.edu.cn>
2021-05-16 22:47:35 +0800

    Reorganized file structures

---
+++ rule/model.py
@@ -0,0 +1,267 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 23:44:50 2021
+
+@author: 高长江
+"""
+
+from rule.tables import genericTable, unitTable, syllableTable, phoneticTable
+from rule.splitors import nameSplitor, unitSplitor, syllableSplitor, phoneticSplitor
+from rule.config import GEN_TOKEN
+from rule.result import Result
+import copy
+
+class GeoTranslator():
+    
+    def __init__(self, tableData):
+        # 四个表格分别是四个类，tableData 包含 4 个字典的数据
+        self.genericTable = genericTable(tableData[0])
+        self.unitTable = unitTable(tableData[1])
+        self.syllableTable = syllableTable(tableData[2])
+        self.phoneticTable = phoneticTable(tableData[3])
+        
+        # 四个拆分器
+        self.nameSplitor = nameSplitor(self.genericTable)
+        self.unitSplitor = unitSplitor(self.unitTable)
+        self.syllableSplitor = syllableSplitor(self.syllableTable)
+        self.phoneticSplitor = phoneticSplitor(self.phoneticTable)
+        
+        # 用于处理未登录词的神经网络（暂时不用）
+        self.network = None
+    
+    def preprocess(self, inputNames: list) -> list:
+        '''
+        Preprocess the input names, including stripping and capitalization etc.
+
+        Parameters
+        ----------
+        inputNames : list(str)
+            Raw input geographic names.
+
+        Returns
+        -------
+        list(str)
+            Preprocessed input geographic names.
+
+        '''
+        processed = []
+        for name in inputNames:
+            # 全部化成小写并去除空格
+            # TODO: 可能需要进一步处理
+            _name = name.strip().lower()
+            _name = _name.replace("'s", "")
+            processed.append(_name)
+        return processed
+    
+    def nameSplit(self, name: str, res: Result) -> list:
+        '''
+        Split the specific name and the generic name.
+
+        Parameters
+        ----------
+        Name : str
+            The preprocessed name.
+
+        Returns
+        -------
+        tuple(list, str) :
+            [0]: index of the main generic name
+            [1]: pairs of generic names and their translations
+        '''
+        idx, spec_pairs, words = self.nameSplitor.split(name)
+        res.saveMainIdx(idx)
+        for pair in spec_pairs:
+            res.saveGen(pair[0], pair[1])
+        res.saveStruct(words)
+        return words
+
+    def unitSplit(self, specName: list) -> list:
+        '''
+        Split the whole specific name into one or several translatable units.
+
+        Parameters
+        ----------
+        specName : list(str)
+            Whole specific names with GEN_TOKEN in list form.
+
+        Returns
+        -------
+        list(str)
+            A list of specific name units.
+        '''
+        units = self.unitSplitor.split(specName)
+        return units
+    
+    def syllabSplit(self, unit: str) -> tuple:
+        '''
+        Split an unknown specific name unit into several syllables together with
+        corresponding phonetics.
+
+        Parameters
+        ----------
+        unit : str
+            a specific name unit
+
+        Returns
+        -------
+        tuple(list(str), list(str))
+            [0]: list of syllables
+            [1]: list of phonetics
+        '''
+        syllables, phonetics = self.syllableSplitor.split(unit)
+        return (syllables, phonetics)
+    
+    def phoneticSplit(self, phonetic: str) -> list:
+        '''
+        Split the phonetics of an unknown syllable into several single phonetics
+
+        Parameters
+        ----------
+        phonetic : str
+            phonetics of an unknown syllable in syllableTable
+
+        Returns
+        -------
+        list(str)
+            list of single phonetics, each is tuple(leading, following)
+
+        '''
+        return self.phoneticSplitor.split(phonetic)
+    
+    def specTranslate(self, spec: list, res: Result):
+        '''
+        Translate the specific name after split from the generic name.
+
+        Parameters
+        ----------
+        specName : list
+            original word list
+            e.g.: ['royal', 'park', 'of', 'london', 'city']
+
+        Returns
+        -------
+        list :
+            Translated specific name parts.
+            e.g.: ['皇家', GEN_TOKEN, '伦敦']
+        '''
+        #units = self.unitSplit(spec)
+
+        #print("units", units)
+        specT = []
+        for i in range(0, len(spec)):
+            word = spec[i]
+            if word == "of":
+                continue
+            else:
+                if i+1 < len(spec) and spec[i+1] == "of":
+                    word += " of"
+            wordT, femaleT = self.unitTranslate(word)
+            #print(unitT)
+            specT.append(wordT)
+            res.saveSpec(word, wordT)
+            if wordT != femaleT:
+                res.saveSpecFemale(word, femaleT)
+        #print("specT:", specT)
+    
+    def unitTranslate(self, unit: str) -> tuple:
+        '''
+        Translate a specific name unit.
+
+        Parameters
+        ----------
+        unit : str
+            specific name unit
+
+        Returns
+        -------
+        str
+            translation of the unit
+        '''
+        #if self.unitTable.inTable(unit): # 如果专名单元在表内
+        #    return self.unitTable.lookup(unit)
+        syllables, phonetics = self.syllabSplit(unit) # 按音节拆分，并得到对应音标片段
+        syllablesT = "" # 存储各个音节的翻译
+        female_syllablesT = ""
+        for syllable, phonetic in zip(syllables, phonetics):
+            # 此处的 phonetic 是音节对应的音标片段
+            #print("syllable", syllable, "phonetic", phonetic)
+            syllable_cap = syllable.capitalize()
+            if syllable == "with" or syllable == "and":
+                syllablesT += "-"
+                female_syllablesT += "-"
+            elif self.unitTable.inTable(syllable_cap): # 音节在表内
+                new_word = self.unitTable.lookup(syllable_cap)
+                syllablesT += new_word
+                female_syllablesT += new_word
+                
+            else: # 音节不在表内
+                singlePhonetics = self.phoneticSplit(phonetic) # 拆分成单音标的列表
+                #print(singlePhonetics)
+                # 4.16.2021
+                front_part = ""
+                prefix = ""
+                prefix_len = 0
+                for letter in syllable:
+                    front_part += letter
+                    if self.syllableTable.inTable(front_part, True):
+                        prefix, prefix_len = self.syllableTable.lookup(front_part, True)
+                syllable_length = len(syllable)
+                back_part = ""
+                suffix = ""
+                suffix_len = 0
+                for i in range(1, syllable_length + 1):
+                    back_part  = syllable[-i] + back_part
+                    if self.syllableTable.inTable(back_part, False):
+                        suffix, suffix_len = self.syllableTable.lookup(back_part, False)
+                singlePhonetics[0] = singlePhonetics[0][prefix_len: len(singlePhonetics[0]) - suffix_len]
+                #最好改一下
+                #4.16.2021
+                #print("singlePhonetics:", singlePhonetics)
+                for word in singlePhonetics:
+                    phonesT = []  # 存储各个音标的翻译
+                    female_phonesT = []
+                    for singlePhonetic in word:
+                        if not self.phoneticTable.inTable(singlePhonetic):
+                        # 假设所有音标都能找到翻译
+                            raise ValueError('Invalid phonetic pair: {}'.format(singlePhonetic))
+                        phonesT.append(self.phoneticTable.lookup(singlePhonetic))
+                    #print('phoneT:', phonesT)
+                    wordT = ""
+                    femaleT = ""
+                    for item in phonesT:
+                        lst = item.split(" ")
+                        wordT += lst[0]
+                        femaleT += lst[-1]
+                    syllablesT += wordT
+                    female_syllablesT += femaleT
+                #4.16.2021
+                syllablesT = prefix + syllablesT + suffix
+                female_syllablesT = prefix + female_syllablesT + suffix
+        #print("syllablesT:", syllablesT)
+        #print("female_syllablesT", female_syllablesT)
+        return (syllablesT, female_syllablesT)
+    
+    def run(self, inputNames: list) -> list:
+        '''
+        Run the translator and generate translation results.
+
+        Parameters
+        ----------
+        inputNames : list(str)
+            Raw input geographic names.
+
+        Returns
+        -------
+        list(str)
+            Translated geographic names.
+
+        '''
+        names = self.preprocess(inputNames)
+        results = []
+        for name in names:
+            res = Result()
+            words = self.nameSplit(name, res)
+            self.specTranslate(words, res)
+            res.merge()
+            results.append(res.getRes())
+        return results
---
+++ rule/splitors.py
@@ -0,0 +1,340 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 21:04:05 2021
+
+@author: 高长江
+"""
+from rule.tables import *
+from rule.config import GEN_TOKEN
+
+import eng_to_ipa as ipa
+
+import torch
+from neural_ipa.nmt_model import NMT
+import sys
+import nltk
+# from neural_ipa.vocab import Vocab
+
+CUDA = torch.cuda.is_available()
+word_ipa_dict = {}
+
+class Splitor():
+    # xxxSplitor 的父类
+    def __init__(self, table):
+        self.table = table
+
+class nameSplitor(Splitor):
+    def split(self, name: str)-> list:
+        '''
+        拆分专名和通名
+
+        Parameters
+        ----------
+        name : str
+            经过预处理的完整地名
+
+        Returns
+        -------
+        list
+            [0]: int，主要通名在单词序列中的位置
+            [1]: list，所有通名以及他们的翻译
+            [3]: list，单词序列（英文）
+
+        '''
+        words = name.split() # 单词的列表
+        possible_gen_idx = [] # indices of possible generic names
+        possible_gen = []
+        ret = [None, None, None] # 返回的列表
+        for i in range(len(words)):
+            if self.table.inTable(words[i]):
+                possible_gen_idx.append(i)
+                possible_gen.append(words[i])
+                
+        def naiveChoose(src_words: list, possibles: list)-> int:
+            # 朴素的确认通名的方法
+            if len(possibles) == 0: # 没有在词典中的通名
+                return -1
+            elif len(possibles) == 1: # 只有一个候选
+                return possibles[0]
+            else: # 不止一个候选
+                if 'of' in src_words and src_words.index('of') - 1 in possibles:
+                    # 如果第一个 of 前的那个单词是可能的通名
+                    # 例如： Royal Park of London City
+                    return possibles[0]
+                else:
+                    # 否则返回最后一个候选
+                    return  possibles[-1]
+                
+        idx_gen = naiveChoose(words, possible_gen_idx)
+        #print("words:", words, "idx_gen:", idx_gen, "possible_gen:", possible_gen)
+        if idx_gen == -1:
+            # raise ValueError('Cannot find generic name in "{}"'.format(name))
+            # 认为没有通名
+            ret[1] = []
+        else:
+            ret[1] = []
+            for i in possible_gen:
+                ret[1].append((i, self.table.lookup(i)))
+        #print(idx_gen)
+        ret[0] = idx_gen
+        ret[2] = words
+        #print("ret", ret)
+        return ret
+
+class unitSplitor(Splitor):
+    def split(self, name: list) -> list:
+        '''
+        拆分专名的可翻译单元（现在只作简单实现）
+
+        Parameters
+        ----------
+        name : list
+            英文单词列表
+
+        Returns
+        -------
+        list(str)
+            可翻译单元的列表，一定为 [part1, GEN_TOKEN, part2] 的格式，
+            part1, part2 可以为空字符串
+
+        '''
+        ret = []
+        idx_gen = name.index(GEN_TOKEN)
+        # 将专名分成 3 部分： GEN_TOKEN 左、本身、右，左右两个部分可以为空字符串
+        left = name[: idx_gen]
+        right = name[idx_gen + 1:]
+        if len(right) > 0 and right[0] == 'of':
+            right.pop(0) # 去掉开头的 of
+        
+        ret.append(' '.join(left))
+        ret.append(GEN_TOKEN)
+        ret.append(' '.join(right))
+        return ret
+
+class syllableSplitor(Splitor):
+    def __init__(self, table):
+        super(syllableSplitor, self).__init__(table)
+        # print("load model from {}".format(model_path), file=sys.stderr)
+        # self.vocab = Vocab.load('vocab.json')
+        self.nmt_model = NMT.load('../neural_ipa/model.bin')
+        if CUDA:
+            self.nmt_model = self.nmt_model.to(torch.device("cuda:0"))
+        self.nmt_model.eval()
+    def predict(self, unit: list)->str:
+        '''
+        预测未登录词的 ipa 音标，需要逐单词翻译成音标
+
+        Parameters
+        ----------
+        unit: List[str]
+            专名单元，由一个或多个单词组成。
+            每个单词都是小写全拼，可以含有 a-z，单引号和连字符
+
+        Returns
+        -------
+        str
+            ipa 音标
+
+        '''
+        words = unit.split() # 按空格分成单个单词
+        ipas = [] # 每个单词对应一个音标
+        for word in words:
+            sent = ' '.join(word).split()
+            hyps = self.nmt_model.beam_search(sent) # beam search 的 5 个结果
+            pred = hyps[0].value # 取可能性最高的
+            # 去掉重音记号
+            for mark in ['ˈ', 'ˌ']:
+                if mark in pred:
+                    pred.remove(mark)
+            ipas.append(''.join(pred)) # 保存音标
+        result = ' '.join(ipas) # 与 ipa_convert 返回格式保持一致
+        return result
+
+    def split(self, unit: str) -> tuple:
+        '''
+        将专名单元按音节拆分
+
+        Parameters
+        ----------
+        unit : str
+            专名单元，可能为 GEN_TOKEN
+
+        Returns
+        -------
+        tuple
+            [0]: 拆分后的音节列表（字母），或 GEN_TOKEN，或空字符串
+            [1]: 拆分后的音节列表（音标），或 GEN_TOKEN，或空字符串
+
+        '''
+        # 找不到办法对应音标和字母音节，暂时不实现音节拆分 
+        if unit == '': # 空字符串，应该很常见
+            return ('', '') # 返回两个空字符串
+        elif unit == GEN_TOKEN: # 保留 token
+            return (GEN_TOKEN, GEN_TOKEN)
+        else: # 正常情况
+            # 跳过附录 C，只进行单词到音标的转换
+            ret = ([], [])
+            can_convert = ipa.isin_cmu(unit)
+            if can_convert:
+                ret[0].append(unit)
+                ret[1].append(ipa.convert(unit, stress_marks='none'))
+                word_ipa_dict[unit] = ipa.convert(unit, stress_marks='none')
+                #print(word_ipa_dict)
+                return ret
+            else: # 正常情况
+                # 跳过附录 C，只进行单词到音标的转换
+                ret = ([], [])
+                can_convert = ipa.isin_cmu(unit)
+                if can_convert:
+                    ret[0].append(unit)
+                    ret[1].append(ipa.convert(unit, stress_marks='none'))
+                    word_ipa_dict[unit] = ipa.convert(unit, stress_marks='none')
+                    #print(word_ipa_dict)
+                    return ret
+                else: # 用模型预测音标
+                    # raise ValueError('Cannot convert unit "{}" to IPA'.format(unit))
+                    ret[0].append(unit)
+                    ret[1].append(self.predict(unit))
+                    return ret
+
+class phoneticSplitor(Splitor):
+    leadings = ['b', 'p', 'd', 't', 'g', 'k', 'v', 'w', 'f',
+                'z', 'dz', 'ts', 's', 'ð', 'θ', 'ʒ', 'ʃ', 'ʤ',
+                'ʧ', 'h', 'm', 'n', 'l', 'r', 'j', 'gw',
+                'kw', 'hw']
+    followings = ['æ', 'ɑ', 'ʌ', 'a',
+                  'ɛ', 'eɪ', 
+                  'ə', 
+                  'i', 'ɪ',
+                  'ɔ', 'o', 'əʊ', 'oʊ',
+                  'u', 'ʊ',
+                  'ju', 'jʊ',
+                  'aɪ', 
+                  'aʊ',
+                  'æn', 'an', 'ʌn', 'æŋ',
+                  'ɑn', 'ʌŋ', 'aʊn', 'ɔn', 'ɔŋ',
+                  'ən', 'əŋ', 'ɛn', 'ɛŋ',
+                  'ɪn', 'in', 'ɪən', 'jən',
+                  'ɪŋ','iŋ',
+                  'un', 'oʊn',
+                  'uŋ']
+    def split(self, phonetic: str) -> list:
+        '''
+        将转换后的国际音标字符串拆分成可查询的元音-辅音组合
+
+        Parameters
+        ----------
+        phonetic : str
+            音标字符串，单词之间有空格，可能为 GEN_TOKEN 或 None
+
+        Returns
+        -------
+        list
+            元辅音组合的列表
+
+        '''
+        def clean(phonetic: str) -> list:
+            # 转换之前还要清洗一下，主要做的事情有：
+            # 1. 把 of 连在前一个词上（假设不会出现两个of）
+            # 2. 词尾和下一个词开头辅音相同，则去除词尾的辅音
+            phonetic = phonetic.replace(' əv', 'əv')
+            phon_list = phonetic.split()
+            for i in range(len(phon_list)):
+                if i >= 1 and phon_list[i][0] in self.leadings \
+                    and  phon_list[i][0] == phon_list[i-1][-1]:
+                    phon_list[i-1] = phon_list[i-1][:-1]
+            return phon_list
+        
+        # phonetic 不可能是 GEN_TOKEN
+        if phonetic == '': # 如果是空字符串
+            return []
+        # 正常情况
+        phon_list = clean(phonetic)
+        ret_list = []
+        for word in phon_list:
+            _word = word # 对 _word 进行多次切除
+            element_list = [] # 用来存储 (leading, following)
+            last_f = '' # 存储上一个 following，针对其以 n 结尾且之后是元音的情况
+            while len(_word) > 0:
+                l = None # leading phonetic
+                f = None # following phonetic
+                heads = []
+                if len(_word) == 1:
+                    heads = [_word[0]]
+                elif len(_word) == 2:
+                    heads = [_word[:2], _word[0]]
+                else:
+                    heads = [_word[:3], _word[:2], _word[0]]
+                # 前三个，前两个或者第一个音标
+                for head in heads:
+                    if head in self.leadings: # 如果以辅音开头
+                        l = head
+                        _word = _word[len(head):] # 切掉 head
+                        break
+                    else: # 检查是因为非法字符还是因为单独元音
+                        if head in self.followings: # 单独元音
+                            if len(last_f) > 0 and last_f[-1] == 'n':
+                                l = 'n'
+                            else:
+                                l = 'none'
+                            break
+                else:
+                    raise ValueError('Invalid leading phonetic: {}'.format(head))
+                # 然后确定 following
+                # 还是考虑前三个，前两个和第一个
+                tails = [] # 注意 _word 此时长度可能为 0
+                if len(_word) == 1:
+                    tails = [_word[0]]
+                elif len(_word) == 2:
+                    tails = [_word[:2], _word[0]]
+                elif len(_word) > 2:
+                    tails = [_word[:3], _word[:2], _word[0]]
+                for tail in tails:
+                    if tail in self.followings:
+                        f = tail
+                        _word = _word[len(tail):] # 切掉找到的 following
+                        break
+                else: # 如果没找到 following，认为是单辅音
+                    f = 'none'
+                
+                element_list.append((l, f)) # 存储找到的 l-f 对
+                last_f = f # 存储 following
+                
+            # 一个单词找完之后
+            #ret_list.extend(element_list)
+            #print("element_list:", element_list)
+            ret_list.append(element_list)
+        # 全部找完以后
+        #print(ret_list)
+        return ret_list
+                    
+                
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

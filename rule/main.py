5a453fa8bfe2a56d79de1ec162d63569d3993a78
RiverGao <181240016@smail.nju.edu.cn>
2021-05-21 15:39:09 +0800

    Add commandline argument

--- rule/main.py
+++ rule/main.py
@@ -8,15 +8,25 @@ from rule.model import GeoTranslator
 from rule.data import readData, printResult, saveData
 from rule.tables import initTables
 
+import sys
 
-
+# 命令行需要指定输入文件，否则默认为 examples.txt
+inputFile = None
+if len(sys.argv) == 1:
+    print('运行方式： python3 main.py [input file]')
+    print('未指定文件，默认读取 examples.txt')
+    inputFile = 'examples.txt'
+elif len(sys.argv) == 2:
+    inputFile = sys.argv[1]
+else:
+    raise RuntimeError('Too many commandline arguments')
 
 # 初始化4个表格：通名表，专名单元表，音节表，音标表
 tableData = initTables()
 # 初始化翻译器
 translator = GeoTranslator(tableData)
 # 输入要翻译的内容
-inputNames = readData('examples.txt')
+inputNames = readData(inputFile)
 # 输出结果
 results = translator.run(inputNames)
 # 打印结果
